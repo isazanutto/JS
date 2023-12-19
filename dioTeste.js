879e5b46d7b32f5db16ec29caa71d235fa319e55
isazanutto <isa_zanutto@hotmail.com>
2023-12-19 17:38:38 -0300

    first commit

---
+++ dioTeste.js
@@ -0,0 +1,233 @@
+let experienciaHeroi = [["Lilith", 1000],
+                        ["Maya",1500],
+                        ["Claptrap",4000],
+                        ["Roland", 6500],
+                        ["Handsome Jack",7200],
+                        ["Troy Calypso",8600],
+                        ["Psycho",9900],
+                        ["Zer0",11000]]
+
+if (experienciaHeroi[0][1] <= 1000) {
+        console.log("O  Herói de nome "+experienciaHeroi[0][0]+" está no nível Ferro. XP="+experienciaHeroi[0][1])
+
+    } else if (experienciaHeroi[0][1] >= 1001 && experienciaHeroi[0][1] <=2000) {
+
+        console.log("O  Herói de nome "+experienciaHeroi[0][0]+" está no nível Bronze. XP="+experienciaHeroi[0][1])
+ } else if(experienciaHeroi[0][1] >= 2001 && experienciaHeroi[0][1] <=5000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[0][0]+" está no nível Prata. XP="+experienciaHeroi[0][1])
+ } else if(experienciaHeroi[0][1] >= 6001 && experienciaHeroi[0][1] <=7000){
+    console.log("O  Herói de nome "+experienciaHeroi[0][0]+" está no nível Ouro. XP="+experienciaHeroi[0][1])
+ } else if(experienciaHeroi[0][1] >= 7001 && experienciaHeroi[0][1] <=8000){
+    console.log("O  Herói de nome "+experienciaHeroi[0][0]+" está no nível Platina. XP="+experienciaHeroi[0][1])
+ }else if(experienciaHeroi[0][1] >= 8001 && experienciaHeroi[0][1] <=9000){
+    console.log("O  Herói de nome "+experienciaHeroi[0][0]+" stá no nível Ascendente. XP="+experienciaHeroi[0][1])
+ }else if(experienciaHeroi[0][1] >= 9001 && experienciaHeroi[0][1] <=10000){
+    console.log("O  Herói de nome "+experienciaHeroi[0][0]+" está no nível Imortal. XP="+experienciaHeroi[0][1])
+ }else {
+    console.log("O  Herói de nome "+experienciaHeroi[0][0]+" está no nível Radiante. XP="+experienciaHeroi[0][1])
+ }
+
+ if (experienciaHeroi[1][1] <= 1000) {
+    console.log("O  Herói de nome "+experienciaHeroi[1][0]+" está no nível Ferro. XP="+experienciaHeroi[1][1])
+
+} else if (experienciaHeroi[1][1] >= 1001 && experienciaHeroi[1][1] <=2000) {
+
+    console.log("O  Herói de nome "+experienciaHeroi[1][0]+" está no nível Bronze. XP="+experienciaHeroi[1][1])
+} else if(experienciaHeroi[1][1] >= 2001 && experienciaHeroi[1][1] <=5000){
+
+console.log("O  Herói de nome "+experienciaHeroi[1][0]+" está no nível Prata. XP="+experienciaHeroi[1][1])
+} else if(experienciaHeroi[1][1] >= 6001 && experienciaHeroi[1][1] <=7000){
+console.log("O  Herói de nome "+experienciaHeroi[1][0]+" está no nível Ouro. XP="+experienciaHeroi[1][1])
+} else if(experienciaHeroi[1][1] >= 7001 && experienciaHeroi[1][1] <=8000){
+console.log("O  Herói de nome "+experienciaHeroi[1][0]+" está no nível Platina. XP="+experienciaHeroi[1][1])
+}else if(experienciaHeroi[1][1] >= 8001 && experienciaHeroi[1][1] <=9000){
+console.log("O  Herói de nome "+experienciaHeroi[1][0]+" stá no nível Ascendente. XP="+experienciaHeroi[1][1])
+}else if(experienciaHeroi[1][1] >= 9001 && experienciaHeroi[1][1] <=10000){
+console.log("O  Herói de nome "+experienciaHeroi[1][0]+" está no nível Imortal. XP="+experienciaHeroi[1][1])
+}else {
+console.log("O  Herói de nome "+experienciaHeroi[1][0]+" está no nível Radiante. XP="+experienciaHeroi[1][1])
+}
+
+if (experienciaHeroi[2][1] <= 1000) {
+    console.log("O  Herói de nome "+experienciaHeroi[2][0]+" está no nível Ferro. XP="+experienciaHeroi[2][1])
+
+} else if (experienciaHeroi[2][1] >= 1001 && experienciaHeroi[2][1] <=2000) {
+
+    console.log("O  Herói de nome "+experienciaHeroi[2][0]+" está no nível Bronze. XP="+experienciaHeroi[2][1])
+
+} else if(experienciaHeroi[2][1] >= 2001 && experienciaHeroi[2][1] <=5000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[2][0]+" está no nível Prata. XP="+experienciaHeroi[2][1])
+
+} else if(experienciaHeroi[2][1] >= 6001 && experienciaHeroi[2][1] <=7000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[2][0]+" está no nível Ouro. XP="+experienciaHeroi[2][1])
+
+} else if(experienciaHeroi[2][1] >= 7001 && experienciaHeroi[2][1] <=8000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[2][0]+" está no nível Platina. XP="+experienciaHeroi[2][1])
+
+}else if(experienciaHeroi[2][1] >= 8001 && experienciaHeroi[2][1] <=9000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[2][0]+" stá no nível Ascendente. XP="+experienciaHeroi[2][1])
+
+}else if(experienciaHeroi[2][1] >= 9001 && experienciaHeroi[2][1] <=10000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[2][0]+" está no nível Imortal. XP="+experienciaHeroi[2][1])
+
+}else {
+    console.log("O  Herói de nome "+experienciaHeroi[2][0]+" está no nível Radiante. XP="+experienciaHeroi[2][1])
+}
+
+if (experienciaHeroi[3][1] <= 1000) {
+    console.log("O  Herói de nome "+experienciaHeroi[3][0]+" está no nível Ferro. XP="+experienciaHeroi[3][1])
+
+} else if (experienciaHeroi[3][1] >= 1001 && experienciaHeroi[3][1] <=2000) {
+
+    console.log("O  Herói de nome "+experienciaHeroi[3][0]+" está no nível Bronze. XP="+experienciaHeroi[3][1])
+
+} else if(experienciaHeroi[3][1] >= 2001 && experienciaHeroi[3][1] <=5000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[3][0]+" está no nível Prata. XP="+experienciaHeroi[3][1])
+
+} else if(experienciaHeroi[3][1] >= 6001 && experienciaHeroi[3][1] <=7000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[3][0]+" está no nível Ouro. XP="+experienciaHeroi[3][1])
+
+} else if(experienciaHeroi[3][1] >= 7001 && experienciaHeroi[3][1] <=8000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[3][0]+" está no nível Platina. XP="+experienciaHeroi[3][1])
+
+}else if(experienciaHeroi[3][1] >= 8001 && experienciaHeroi[3][1] <=9000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[3][0]+" stá no nível Ascendente. XP="+experienciaHeroi[3][1])
+
+}else if(experienciaHeroi[3][1] >= 9001 && experienciaHeroi[3][1] <=10000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[3][0]+" está no nível Imortal. XP="+experienciaHeroi[3][1])
+
+}else {
+    console.log("O  Herói de nome "+experienciaHeroi[3][0]+" está no nível Radiante. XP="+experienciaHeroi[4][1])
+}
+if (experienciaHeroi[4][1] <= 1000) {
+    console.log("O  Herói de nome "+experienciaHeroi[4][0]+" está no nível Ferro. XP="+experienciaHeroi[4][1])
+
+} else if (experienciaHeroi[4][1] >= 1001 && experienciaHeroi[4][1] <=2000) {
+
+    console.log("O  Herói de nome "+experienciaHeroi[4][0]+" está no nível Bronze. XP="+experienciaHeroi[4][1])
+
+} else if(experienciaHeroi[4][1] >= 2001 && experienciaHeroi[4][1] <=5000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[4][0]+" está no nível Prata. XP="+experienciaHeroi[4][1])
+
+} else if(experienciaHeroi[4][1] >= 6001 && experienciaHeroi[4][1] <=7000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[4][0]+" está no nível Ouro. XP="+experienciaHeroi[4][1])
+
+} else if(experienciaHeroi[4][1] >= 7001 && experienciaHeroi[4][1] <=8000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[4][0]+" está no nível Platina. XP="+experienciaHeroi[4][1])
+
+}else if(experienciaHeroi[4][1] >= 8001 && experienciaHeroi[4][1] <=9000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[4][0]+" stá no nível Ascendente. XP="+experienciaHeroi[4][1])
+
+}else if(experienciaHeroi[4][1] >= 9001 && experienciaHeroi[4][1] <=10000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[4][0]+" está no nível Imortal. XP="+experienciaHeroi[4][1])
+
+}else {
+    console.log("O  Herói de nome "+experienciaHeroi[4][0]+" está no nível Radiante. XP="+experienciaHeroi[4][1])
+}
+
+if (experienciaHeroi[5][1] <= 1000) {
+    console.log("O  Herói de nome "+experienciaHeroi[5][0]+" está no nível Ferro. XP="+experienciaHeroi[5][1])
+
+} else if (experienciaHeroi[5][1] >= 1001 && experienciaHeroi[5][1] <=2000) {
+
+    console.log("O  Herói de nome "+experienciaHeroi[5][0]+" está no nível Bronze. XP="+experienciaHeroi[5][1])
+
+} else if(experienciaHeroi[5][1] >= 2001 && experienciaHeroi[5][1] <=5000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[5][0]+" está no nível Prata. XP="+experienciaHeroi[5][1])
+
+} else if(experienciaHeroi[5][1] >= 6001 && experienciaHeroi[5][1] <=7000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[5][0]+" está no nível Ouro. XP="+experienciaHeroi[5][1])
+
+} else if(experienciaHeroi[5][1] >= 7001 && experienciaHeroi[5][1] <=8000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[5][0]+" está no nível Platina. XP="+experienciaHeroi[5][1])
+
+}else if(experienciaHeroi[5][1] >= 8001 && experienciaHeroi[5][1] <=9000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[5][0]+" stá no nível Ascendente. XP="+experienciaHeroi[5][1])
+
+}else if(experienciaHeroi[5][1] >= 9001 && experienciaHeroi[5][1] <=10000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[5][0]+" está no nível Imortal. XP="+experienciaHeroi[5][1])
+
+}else {
+    console.log("O  Herói de nome "+experienciaHeroi[5][0]+" está no nível Radiante. XP="+experienciaHeroi[5][1])
+}
+if (experienciaHeroi[6][1] <= 1000) {
+    console.log("O  Herói de nome "+experienciaHeroi[6][0]+" está no nível Ferro. XP="+experienciaHeroi[6][1])
+
+} else if (experienciaHeroi[6][1] >= 1001 && experienciaHeroi[6][1] <=2000) {
+
+    console.log("O  Herói de nome "+experienciaHeroi[6][0]+" está no nível Bronze. XP="+experienciaHeroi[6][1])
+
+} else if(experienciaHeroi[6][1] >= 2001 && experienciaHeroi[6][1] <=5000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[6][0]+" está no nível Prata. XP="+experienciaHeroi[6][1])
+
+} else if(experienciaHeroi[6][1] >= 6001 && experienciaHeroi[6][1] <=7000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[6][0]+" está no nível Ouro. XP="+experienciaHeroi[6][1])
+
+} else if(experienciaHeroi[6][1] >= 7001 && experienciaHeroi[6][1] <=8000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[6][0]+" está no nível Platina. XP="+experienciaHeroi[6][1])
+
+}else if(experienciaHeroi[6][1] >= 8001 && experienciaHeroi[6][1] <=9000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[6][0]+" stá no nível Ascendente. XP="+experienciaHeroi[6][1])
+
+}else if(experienciaHeroi[6][1] >= 9001 && experienciaHeroi[6][1] <=10000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[6][0]+" está no nível Imortal. XP="+experienciaHeroi[6][1])
+
+}else {
+    console.log("O  Herói de nome "+experienciaHeroi[6][0]+" está no nível Radiante. XP="+experienciaHeroi[6][1])
+}
+if (experienciaHeroi[7][1] <= 1000) {
+    console.log("O  Herói de nome "+experienciaHeroi[7][0]+" está no nível Ferro. XP="+experienciaHeroi[7][1])
+
+} else if (experienciaHeroi[7][1] >= 1001 && experienciaHeroi[7][1] <=2000) {
+
+    console.log("O  Herói de nome "+experienciaHeroi[7][0]+" está no nível Bronze. XP="+experienciaHeroi[7][1])
+
+} else if(experienciaHeroi[7][1] >= 2001 && experienciaHeroi[7][1] <=5000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[7][0]+" está no nível Prata. XP="+experienciaHeroi[7][1])
+
+} else if(experienciaHeroi[7][1] >= 6001 && experienciaHeroi[7][1] <=7000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[7][0]+" está no nível Ouro. XP="+experienciaHeroi[7][1])
+
+} else if(experienciaHeroi[7][1] >= 7001 && experienciaHeroi[7][1] <=8000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[7][0]+" está no nível Platina. XP="+experienciaHeroi[7][1])
+
+}else if(experienciaHeroi[7][1] >= 8001 && experienciaHeroi[7][1] <=9000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[7][0]+" stá no nível Ascendente. XP="+experienciaHeroi[7][1])
+
+}else if(experienciaHeroi[7][1] >= 9001 && experienciaHeroi[7][1] <=10000){
+
+    console.log("O  Herói de nome "+experienciaHeroi[7][0]+" está no nível Imortal. XP="+experienciaHeroi[7][1])
+
+}else {
+    console.log("O  Herói de nome "+experienciaHeroi[7][0]+" está no nível Radiante. XP="+experienciaHeroi[7][1])
+}
